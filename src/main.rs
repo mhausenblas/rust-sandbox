db7569858483d4744b8c92cfeb1f49f060e26539
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-11-18 06:12:37 +0000

    updates main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-fn main() {
-    let needle = 42;
-    let haystack = [1, 1, 2, 4, 42, 423, 500];
+extern crate reqwest;
 
-    for item in haystack.iter() {
-        if item == &needle {
-            println!("Found ya: {}", item);
-        }
+use std::io::Read;
+
+fn main() {
+    let mut response =
+        reqwest::get("https://httpbin.org/status/418").expect("Failed to send request");
+    println!("{}", response.status());
+    for header in response.headers().iter() {
+        println!("{:?}", header);
     }
+    let mut buf = String::new();
+    response
+        .read_to_string(&mut buf)
+        .expect("Failed to read response");
+    println!("{}", buf);
 }
